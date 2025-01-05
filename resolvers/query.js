e0c64e9c42e8383c78050d62122522529268b52e
saJeremyQin <343220868@qq.com>
2025-01-05 10:46:16 +1030

    add console.error in dishes resolvers

--- resolvers/query.js
+++ resolvers/query.js
@@ -3,7 +3,12 @@
 module.exports = {
     hello: () => 'Hello world!',
     dishes: async (parent, args, {models}) => {
-        return await models.Dish.find();
+        try {
+            return await models.Dish.find();  
+        } catch (error) {
+            console.error("Error fetching dishes", error);
+            return [];
+        }
     },
     // getDish by ID
     getDish: async (parent, args, {models}) => {
